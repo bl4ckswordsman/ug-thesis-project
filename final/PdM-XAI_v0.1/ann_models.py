ff2e9a30a9fce9f5fdf42c51ec5bf3e249e92488
amarildo <amarildo.rajta@gmail.com>
2024-05-17 18:29:23 +0200

    remove commented out code

--- final/PdM-XAI_v0.1/ann_models.py
+++ final/PdM-XAI_v0.1/ann_models.py
@@ -128,25 +128,3 @@ def create_and_train_model(
 
     return model, model_name
 
-# def create_and_train_model(x_train, y_train, num_classes, fold):
-#     # Define the model inside the function
-#     model = Sequential1()
-#     model.add(Input(shape=(x_train.shape[1],)))  # Input layer
-#     model.add(Dense(32, activation='relu'))  # Hidden layer 1, 32 neurons, ReLU(Rectified Linear Unit) activation
-#     model.add(Dense(64, activation='relu'))  # Hidden layer 2
-#     model.add(Dense(num_classes, activation='softmax'))  # Output layer
-#
-#     # Compile the model
-#     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-#
-#     # Train the model and save the history
-#     history = model.fit(x_train, y_train, epochs=epochs_nr, batch_size=batch_size_nr)
-#
-#     # Convert the history.history dict to a pandas DataFrame
-#     hist_df = pd.DataFrame(history.history)
-#
-#     # Save to csv
-#     model_name = type(model).__name__
-#     hist_df.to_csv(f'results/{model_name}/csv/history_fold_{fold}.csv')
-#
-#     return model, model_name
