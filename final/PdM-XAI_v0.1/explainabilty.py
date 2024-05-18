b698edbb22949cd2c6b4cd9e7ccaa035e64878ad
amarildo <amarildo.rajta@gmail.com>
2024-05-18 20:08:19 +0200

    untested XAI lime method

--- final/PdM-XAI_v0.1/explainabilty.py
+++ final/PdM-XAI_v0.1/explainabilty.py
@@ -1,5 +1,7 @@
 import numpy as np
 from sklearn.inspection import permutation_importance
+from lime import lime_tabular
+from sklearn.metrics import accuracy_score
 
 from plotting import plot_feature_importances
 
@@ -7,6 +9,11 @@ from plotting import plot_feature_importances
 def keras_score(estimator, X, y):
     loss, accuracy = estimator.evaluate(X, y)
     return accuracy
+
+
+def sklearn_score(estimator, X, y):
+    y_pred = estimator.predict(X)
+    return accuracy_score(y, y_pred)
 
 
 def calculate_pfi(model, x_val, y_val, scoring):
@@ -50,3 +57,31 @@ def explain_model_with_pfi(model, model_name, x_val, y_val, fold, scoring,
 
     # Return importances, standard deviations, and indices
     return importances, std, indices
+
+
+def explain_model_with_lime(model, model_name, x_val, y_val, fold, feature_names, all_explanations=None):
+    # Create a LimeTabularExplainer
+    explainer = lime_tabular.LimeTabularExplainer(x_val, feature_names=feature_names, class_names=['0', '1'],
+                                                  verbose=True, mode='classification')
+
+    if model is not None:
+        # Explain a prediction
+        explanation = explainer.explain_instance(x_val[0], model.predict_proba, num_features=5)
+
+        # If all_explanations is provided, append the current explanation
+        if all_explanations is not None:
+            all_explanations.append(explanation)
+
+    # If fold is None, it means we want to plot the combined explanations
+    if fold is None and all_explanations is not None:
+        # Calculate mean explanations across all folds
+        mean_explanations = np.mean(all_explanations, axis=0)
+
+        # Plot the explanations
+        plot_feature_importances(mean_explanations, feature_names, model_name, 'combined')
+
+        # Reset explanations to None
+        explanations = None
+
+    # Return explanations
+    return explanations
